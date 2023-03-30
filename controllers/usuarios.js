e7a7a9fccf6d85bb75e9b9d39f029818605900ea
Everth Paredes <epe0413@gmail.com>
2023-03-30 12:17:17 -0600

    Validar campos obligatorios - Email

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,7 @@
 const { response, request } =require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -18,10 +19,21 @@ const usuariosGet = (req = request, res = response) => {
 
 const usuariosPost = async (req, res = response) => {
 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors);
+    }
+
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario( { nombre, correo, password, rol } );
 
     // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({ correo });
+    if ( existeEmail ){
+        return res.status(400).json({
+            msg: 'El correo ya existe'
+        })
+    }
     
 
     // Encriptar la contraseña
