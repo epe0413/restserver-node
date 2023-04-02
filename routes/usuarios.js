a93a7090dc575c4611135dfd9036759a1c54656d
Everth Paredes <epe0413@gmail.com>
2023-04-02 15:44:18 -0600

    Validar todos los campos necesarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,13 +5,18 @@ const { usuariosGet,
         usuariosPut, 
         usuariosPatch, 
         usuariosDelete } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', usuariosGet);
 router.put('/:id', usuariosPut);
 router.post('/',[
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password debe tener más de 6 letras').isLength({ min:6 }),
         check('correo', 'El correo no es válido').isEmail(),
+        check('rol', 'No es un rol permitido').isIn('ADMIN_ROLE', 'USER_ROL'),
+        validarCampos
 ],usuariosPost);
 router.patch('/', usuariosPatch);
 router.delete('/', usuariosDelete);
