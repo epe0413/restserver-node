3d5bca8d89dc8829b851c6dc5caa8b79bc894fba
Everth Paredes <epe0413@gmail.com>
2023-04-02 20:06:04 -0600

    Validaciones adicionales en el PUT

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,12 +8,17 @@ const { usuariosGet,
         usuariosDelete } = require('../controllers/usuarios');
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { esRolValido, emailExiste } = require('../helpers/db-validators');
+const { esRolValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const router = Router();
 
 router.get('/', usuariosGet);
-router.put('/:id', usuariosPut);
+router.put('/:id',[
+        check('id', 'No es un ID válido').isMongoId(),
+        check('id').custom(existeUsuarioPorId),
+        check('rol').custom( esRolValido ),
+        validarCampos
+], usuariosPut);
 router.post('/',[
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('password', 'El password debe tener más de 6 letras').isLength({ min:6 }),
