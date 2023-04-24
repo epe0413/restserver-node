c4e5092aca05c44c06cf7acc7014264a9399c7d0
Everth Paredes <epe0413@gmail.com>
2023-04-24 10:41:22 -0600

    Crear una categoria

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT, validarCampos } = require('../middlewares');
+const { crearCategoria } = require('../controllers/categorias');
 
 const router = Router();
 
@@ -20,9 +21,11 @@ router.get('/:id', (req, res) => {
 })
 
 // Crear categoría -> privado - cualquier persona con un token válido
-router.post('/', (req, res) => {
-    res.json('post');
-})
+router.post('/', [
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    validarCampos
+], crearCategoria);
 
 // Actualizar categoría -> privado - cualquier persona con un token válido
 router.put('/:id', (req, res) => {
