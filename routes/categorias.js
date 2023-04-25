c831d10f3ccecb1ad2c0007a89d737253af86e4a
Everth Paredes <epe0413@gmail.com>
2023-04-24 22:24:49 -0600

    CRUD de Categorías

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarJWT, validarCampos } = require('../middlewares');
-const { crearCategoria } = require('../controllers/categorias');
+const { validarJWT, validarCampos, tieneRole, esAdminRole } = require('../middlewares');
+const { crearCategoria,
+    obtenerCategoria,
+    obtenerCategorias,
+    actualizarCategoria,
+    borrarCategoria } = require('../controllers/categorias');
+const { existeCategoriaPorId } = require('../helpers/db-validators');
 
 const router = Router();
 
@@ -11,14 +16,14 @@ const router = Router();
  */
 
 // Obtener todas las categorias -> publico
-router.get('/', (req, res) => {
-    res.json('GET');
-})
+router.get('/', obtenerCategorias)
 
 // Obtener todas una categoria por ID -> publico
-router.get('/:id', (req, res) => {
-    res.json('GET - id');
-})
+router.get('/:id', [
+    check('id', 'No es un id valido').isMongoId(),
+    check('id').custom( existeCategoriaPorId ),
+    validarCampos
+], obtenerCategoria);
 
 // Crear categoría -> privado - cualquier persona con un token válido
 router.post('/', [
@@ -28,13 +33,20 @@ router.post('/', [
 ], crearCategoria);
 
 // Actualizar categoría -> privado - cualquier persona con un token válido
-router.put('/:id', (req, res) => {
-    res.json('put');
-})
+router.put('/:id',[
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('id').custom( existeCategoriaPorId ),
+    validarCampos
+], actualizarCategoria)
 
 // Borrar categoría -> privado - Admin
-router.delete('/:id', (req, res) => {
-    res.json('delete');
-})
+router.delete('/:id',[
+    validarJWT,
+    esAdminRole,
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom(existeCategoriaPorId),
+    validarCampos
+],borrarCategoria)
 
 module.exports = router;
