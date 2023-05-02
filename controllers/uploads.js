0030a43601fb757895e62f31e11db26cfb24ab98
Everth Paredes <epe0413@gmail.com>
2023-05-01 21:15:34 -0600

    Subir archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,10 +1,26 @@
+const path = require('path')
+
 const { response } = require("express");
 
 
 const cargarArchivo = (req, res = response) => {
-    return res.json({
-        msg: 'Upload'
-    })
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
+        res.status(400).json({msg: 'No hay archivos que subir'});
+        return;
+    }
+
+    const {archivo} = req.files;
+
+    const uploadPath = path.join( __dirname, '../uploads/', archivo.name);
+
+    archivo.mv(uploadPath, function(err) {
+    if (err) {
+        return res.status(500).json({err});
+    }
+
+    res.json({msg: 'File uploaded to ' + uploadPath});
+    });
 }
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 const { dbConnection } = require('../database/config');
 
 class Server{
@@ -44,6 +46,12 @@ class Server{
 
         // Directorio publico
         this.app.use( express.static('public') );
+
+        // FileUpload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        }));
     }
 
     routes() {
