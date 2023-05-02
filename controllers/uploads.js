82b685b9426c548159c7c4a4bed65ed269f67d94
Everth Paredes <epe0413@gmail.com>
2023-05-02 15:46:18 -0600

    Validar la extensión

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -11,16 +11,29 @@ const cargarArchivo = (req, res = response) => {
     }
 
     const {archivo} = req.files;
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length -1];
 
-    const uploadPath = path.join( __dirname, '../uploads/', archivo.name);
+    // Validar la extension
+    const extensionesValidas =[ 'png', 'jpg', 'jpeg' ]
 
-    archivo.mv(uploadPath, function(err) {
-    if (err) {
-        return res.status(500).json({err});
+    if( !extensionesValidas.includes(extension)){
+        return res.status(400).json({
+            msg: `La extension ${ extension } no es permitida, solo se admiten ${ extensionesValidas }`
+        })
     }
 
-    res.json({msg: 'File uploaded to ' + uploadPath});
-    });
+    res.json({ extension })
+
+    // const uploadPath = path.join( __dirname, '../uploads/', archivo.name);
+
+    // archivo.mv(uploadPath, function(err) {
+    // if (err) {
+    //     return res.status(500).json({err});
+    // }
+
+    // res.json({msg: 'File uploaded to ' + uploadPath});
+    // });
 }
 
 module.exports = {
